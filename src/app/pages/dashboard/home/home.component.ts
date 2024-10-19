917a59f5b0ef6f1f42acf51715f3e15835b32747
JesusOyola <jesus.oyola@interatica.com>
2024-10-19 19:47:57 -0300

    feature(home): Add buttons for actions and search filter was adapted to the service

--- src/app/pages/dashboard/home/home.component.ts
+++ src/app/pages/dashboard/home/home.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MaterialModule } from '../../../shared/material/material.module';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Hero } from '../../../shared/interface/hero';
+import { MaterialModule } from '../../../shared/material/material.module';
 import { HeroesService } from '../../../shared/service/heroes.service';
 
 @Component({
@@ -42,7 +42,10 @@ export default class HomeComponent {
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    const filteredHeroes = this.heroService.searchHeroesByName(
+      filterValue.trim().toLowerCase()
+    );
+    this.dataSource.data = filteredHeroes;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
