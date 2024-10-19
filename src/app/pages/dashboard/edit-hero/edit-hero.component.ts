6a53c22df4165f50d97413720fef696288f4c749
JesusOyola <jesus.oyola@interatica.com>
2024-10-19 13:31:17 -0300

    feature(folder): create general folder structure and first components

---
+++ src/app/pages/dashboard/edit-hero/edit-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-hero.component.html',
+  styleUrl: './edit-hero.component.scss'
+})
+export class EditHeroComponent {
+
+}
