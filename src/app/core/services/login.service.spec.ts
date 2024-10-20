589d628d175e218d6d230b0cbfb0351ec0d0c79f
JesusOyola <jesus.oyola@interatica.com>
2024-10-20 20:23:17 -0300

    feature(tests): LoginService done

--- src/app/core/services/login.service.spec.ts
+++ src/app/core/services/login.service.spec.ts
@@ -13,4 +13,30 @@ describe('LoginService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should return an empty string initially', (done: DoneFn) => {
+    service.getUser().subscribe((user) => {
+      expect(user).toBe('');
+      done();
+    });
+  });
+
+  it('should update the user data', (done: DoneFn) => {
+    const testUser = 'Superman';
+    service.setUser(testUser);
+    service.getUser().subscribe((user) => {
+      expect(user).toBe(testUser);
+      done();
+    });
+  });
+
+  it('should allow multiple updates to user data', (done: DoneFn) => {
+    const firstUser = 'Spiderman';
+    const secondUser = 'Ironman';
+    service.setUser(firstUser);
+    service.setUser(secondUser);
+    service.getUser().subscribe((user) => {
+      expect(user).toBe(secondUser);
+      done();
+    });
+  });
 });
